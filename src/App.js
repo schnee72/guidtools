f6700a3b9432a9a0ef4a16b0e98bb1e3d12a7b1f
Dustin Schnee <schnee72@gmail.com>
2017-09-05 21:28:54 -0500

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import { h, Component } from 'preact';
+import { h } from 'preact';
 import Generate from './Generate';
 import Validate from './Validate';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>guids</h1>
-        <Generate />
-        <Validate />
-        <div class="float-left">
-          <a href="https://github.com/schnee72/guidtools">github</a>
-        </div>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+  <h1>guids</h1>
+  <Generate />
+  <Validate />
+  <div class="float-left">
+    <a href="https://github.com/schnee72/guidtools">github</a>
+  </div>
+</div>
+);
+
+export default App;
